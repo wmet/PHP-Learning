63e069b329b4e82d86de578075117e27d054144b
wmet <wme2018@gmail.com>
2017-07-17 16:13:50 +0800

    contents deleted comments

--- ContainerTest.php
+++ ContainerTest.php
@@ -75,15 +75,3 @@ $container->bind('IMessage',function(){
 
 Route::get('callindex','ControllerA@index'); //main start
 
-
-
-/*
-$reclass=new ReflectionClass('ControllerA');// 获取反射类
-$method=$reclass->getMethod('index');//index 方法
-$param=$method->getParameters(); //index 方法的所有参数
-$methodparamClassName=$param[0]->getClass()->getName(); //Imessage 第一个参数的 类名
-$paramInstance=$container->make('IMessage');// 从容器中取这个类名绑定的实例
-
-$controllerInstance=$reclass->newInstance();//$a=new ControllerA
-$method->invoke($controllerInstance,$paramInstance);//执行$a->index
-*/
